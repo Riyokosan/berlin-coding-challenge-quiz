80e2218faee538bda055f2cb78bb6bc7fa85dee3
Riyokosan <Riyokosan@gmail.com>
2018-05-07 10:56:08 +0200

    Images Commit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.onload = init();
 function init() {
     var questions = [{
             question: "What is the capital of Germany?",
+            src: "images/Germany.jpg",
             answers: [
                 { text: "Amsterdam" },
                 { text: "Berlin", isCorrect: true },
@@ -11,38 +12,41 @@ function init() {
         },
         {
             question: "What is the capital of Morocco?",
+            src: "images/Morocco.png",
             answers: [
                 { text: "Nouakchott" },
                 { text: "Rabat", isCorrect: true },
                 { text: "Tripoli" },
             ]
         },
-        {
-            question: "What is the capital of Estonia?",
-            answers: [
-                { text: "Riga" },
-                { text: "Tallinn", isCorrect: true },
-                { text: "Vilnius" },
-            ]
-        },
+        // {
+        //     question: "What is the capital of Estonia?",
+        //     answers: [
+        //         { text: "Riga" },
+        //         { text: "Tallinn", isCorrect: true },
+        //         { text: "Vilnius" },
+        //     ]
+        // },
         {
             question: "What is the capital of Australia?",
+            src: "images/Australia.jpg",
             answers: [
                 { text: "Canberra", isCorrect: true },
                 { text: "Melbourne" },
                 { text: "Sydney" },
             ]
         },
-        {
-            question: "What is the capital of Canada?",
-            answers: [
-                { text: "Ottawa", isCorrect: true },
-                { text: "Toronto" },
-                { text: "Vancouver" },
-            ]
-        },
+        // {
+        //     question: "What is the capital of Canada?",
+        //     answers: [
+        //         { text: "Ottawa", isCorrect: true },
+        //         { text: "Toronto" },
+        //         { text: "Vancouver" },
+        //     ]
+        // },
         {
             question: "What is the capital of Paraguay?",
+            src: "images/Paraguay.png",
             answers: [
                 { text: "Asunción", isCorrect: true },
                 { text: "La Paz" },
@@ -51,6 +55,7 @@ function init() {
         },
         {
             question: "What is the capital of Vietnam?",
+            src: "images/Vietnam.jpg",
             answers: [
                 { text: "Hanoi", isCorrect: true },
                 { text: "Ho Chi Minh City" },
@@ -59,22 +64,24 @@ function init() {
         },
         {
             question: "What is the capital of India?",
+            src: "images/India.png",
             answers: [
                 { text: "Bangalore" },
                 { text: "Bombay" },
                 { text: "New Delhi", isCorrect: true },
             ]
         },
-        {
-            question: "What is the capital of Iraq?",
-            answers: [
-                { text: "Baghdad", isCorrect: true },
-                { text: "Damas" },
-                { text: "Tehran" },
-            ]
-        },
+        // {
+        //     question: "What is the capital of Iraq?",
+        //     answers: [
+        //         { text: "Baghdad", isCorrect: true },
+        //         { text: "Damas" },
+        //         { text: "Tehran" },
+        //     ]
+        // },
         {
             question: "What is the capital of Colombia?",
+            src: "images/Colombia.jpg",
             answers: [
                 { text: "Bogotá", isCorrect: true },
                 { text: "Caracas" },
@@ -88,6 +95,7 @@ function init() {
     function updateQuestionAndScore() {
         if (iQuestion >= questions.length) {
             document.querySelector('#question')[0].outerHTML = '';
+            document.querySelector('img')[0].removeAttribute(src);
             document.querySelector('.answer')[2].outerHTML = '';
             document.querySelector('.answer')[1].outerHTML = '';
             document.querySelector('.answer')[0].outerHTML = '';
